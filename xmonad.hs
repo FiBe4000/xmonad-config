4aeae3920fc068720c7b6222bfe15728d8e5dfa8
FiBe4000 <fibeub@gmail.com>
2018-12-07 18:10:58 +0100

    Fix mod key settings and add window spacing toggle

--- xmonad.hs
+++ xmonad.hs
@@ -15,6 +15,7 @@ import XMonad.Layout.LimitWindows
 import XMonad.Layout.GridVariants
 import XMonad.Layout.NoBorders
 import XMonad.Layout.MultiColumns
+import XMonad.Layout.Spacing
 import qualified XMonad.Layout.Fullscreen as F
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
@@ -28,7 +29,7 @@ myTerminal = "urxvtc"
 
 myNormalBorderColor  = "#073642"
 myFocusedBorderColor = "#268bd2"
-myBorderWidth        = 0
+myBorderWidth        = 1
 
 -- Workspaces
 myWorkspaces = ["1 - Web", "2 - Terminals", "3 - Code", "4 - Games", "5 - Chat", "6 - Music", "7 - Media", "8 - Etc1", "9 - Etc2"]
@@ -45,26 +46,28 @@ myManageHook = composeAll . concat $
     , [className =? "yakyak"        --> doShift "5 - Chat"]
     , [className =? "Kodi"          --> doShift "7 - Media"]
     , [className =? "Kodi"          --> doFullFloat]
-    , [className =? "Xmessage"      --> doFloat]
+    , [className =? "Xmessage"      --> doCenterFloat]
     , [isFullscreen --> (doF W.focusUp <+> doFullFloat)]
   ]
 
-myLayout     =  onWorkspaces ["1- Web", "3 - Code", "4 - Mail", "6 - Music", "7 - Media", "8 - Etc1", "9 - Etc2"] customLayout $
+myLayout     =  avoidStruts $ smartBorders $ spacingRaw False (Border 0 5 5 5) False (Border 5 5 5 5) False  $
+                onWorkspaces ["1- Web", "3 - Code", "4 - Mail", "6 - Music", "7 - Media", "8 - Etc1", "9 - Etc2"] customLayout $
                 onWorkspaces ["2 - Terminals"] terminalLayout $
                 onWorkspaces ["5 - Chat"] chatLayout $
                 onWorkspaces ["7 - Media"] mediaLayout $
                 customLayout
 
-customLayout    = avoidStruts $ noBorders $ layoutHook defaultConfig
-terminalLayout  = avoidStruts $ noBorders $ TallGrid 2 2 (2/3) (16/10) (5/100) ||| customLayout
-chatLayout      = avoidStruts $ noBorders $ customLayout ||| multiCol [1] 2 (3/100) (1/5) ||| Tall 1 (3/100) (1/5)
-mediaLayout     = noBorders $ F.fullscreenFull Full
+customLayout    = layoutHook defaultConfig
+terminalLayout  = TallGrid 2 2 (2/3) (16/10) (5/100) ||| customLayout
+chatLayout      = customLayout ||| multiCol [1] 2 (3/100) (1/5) ||| Tall 1 (3/100) (1/5)
+mediaLayout     = F.fullscreenFull Full
 
 fadeInactiveLogHook' = fadeOutLogHook . fadeIf (isUnfocused <&&> (propertyToQuery (Not (ClassName "Google-chrome") `And` (Not (ClassName "Kodi")))))
 
 myLogHook :: X ()
-myLogHook = fadeInactiveLogHook' fadeAmount >> ewmhDesktopsLogHook
-    where fadeAmount = 0.6
+--myLogHook = fadeInactiveLogHook' fadeAmount >> ewmhDesktopsLogHook
+    --where fadeAmount = 0.6
+myLogHook = ewmhDesktopsLogHook
 
 myStartupHook = do
   ewmhDesktopsStartup
@@ -123,10 +126,19 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
 
     -- Increment the number of windows in the master area
-    , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
+    , ((modMask,               xK_comma ), sendMessage (IncMasterN 1))
 
     -- Decrement the number of windows in the master area
-    , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
+    , ((modMask,               xK_period), sendMessage (IncMasterN (-1)))
+
+    -- Increment window spacing
+    , ((modMask .|. shiftMask, xK_l),      incWindowSpacing 1)
+
+    -- Decrement window spacing
+    , ((modMask .|. shiftMask, xK_h),      decWindowSpacing 1)
+
+    -- Toggle window spacing
+    , ((myModMask .|. shiftMask, xK_s),      toggleWindowSpacingEnabled)
 
     -- toggle the status bar gap
     --, ((modMask              , xK_b     ),
@@ -162,22 +174,22 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
    -- Additional bindings
    [
-    ((mod1Mask .|. controlMask , xK_l),               spawn "sxlock -f \"-misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1\""),
-    ((mod1Mask .|. controlMask .|. shiftMask , xK_q), spawn "shutdown now"),
-    ((mod1Mask .|. controlMask .|. shiftMask , xK_r), spawn "reboot"),
-    ((mod1Mask .|. controlMask .|. shiftMask , xK_w), spawn "/home/fibe/.xmonad/scripts/setwallpaper.sh"),
-    ((shiftMask , xF86XK_MonBrightnessDown),          spawn "xbacklight -dec 5"),
-    ((shiftMask , xF86XK_MonBrightnessUp),            spawn "xbacklight -inc 5"),
-    ((0 , xF86XK_MonBrightnessDown),                  spawn "xbacklight -dec 10"),
-    ((0 , xF86XK_MonBrightnessUp),                    spawn "xbacklight -inc 10"),
-    ((0 , xF86XK_KbdBrightnessDown),                  spawn "asus-kbd-backlight down"),
-    ((0 , xF86XK_KbdBrightnessUp),                    spawn "asus-kbd-backlight up"),
-    ((0 , xF86XK_AudioLowerVolume),                   spawn "/usr/bin/pulseaudio-ctl down"),--"amixer set Master on && amixer set Headphone on && amixer set Master 2-"),
-    ((0 , xF86XK_AudioRaiseVolume),                   spawn "/usr/bin/pulseaudio-ctl up"),--"amixer set Master on && amixer set Headphone on && amixer set Master 2+"),
-    ((0 , xF86XK_AudioMute),                          spawn "/usr/bin/pulseaudio-ctl mute"),--"amixer set Master toggle && amixer set Headphone on && amixer set Speaker on"),
-    ((mod4Mask , xK_space),                           spawn "/home/fibe/.xmonad/scripts/switchpowgov.sh"),
-    ((0 , xK_Print),                                  spawn "scrot"),
-    ((mod1Mask , xK_f),                               spawn "spacefm")
+    ((modMask .|. controlMask , xK_l),               spawn "sxlock -f \"-misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1\""),
+    ((modMask .|. controlMask .|. shiftMask , xK_q), spawn "shutdown now"),
+    ((modMask .|. controlMask .|. shiftMask , xK_r), spawn "reboot"),
+    ((modMask .|. controlMask .|. shiftMask , xK_w), spawn "/home/fibe/.xmonad/scripts/setwallpaper.sh"),
+    ((shiftMask , xF86XK_MonBrightnessDown),         spawn "xbacklight -dec 5"),
+    ((shiftMask , xF86XK_MonBrightnessUp),           spawn "xbacklight -inc 5"),
+    ((0 , xF86XK_MonBrightnessDown),                 spawn "xbacklight -dec 10"),
+    ((0 , xF86XK_MonBrightnessUp),                   spawn "xbacklight -inc 10"),
+    ((0 , xF86XK_KbdBrightnessDown),                 spawn "asus-kbd-backlight down"),
+    ((0 , xF86XK_KbdBrightnessUp),                   spawn "asus-kbd-backlight up"),
+    ((0 , xF86XK_AudioLowerVolume),                  spawn "/usr/bin/pulseaudio-ctl down"),--"amixer set Master on && amixer set Headphone on && amixer set Master 2-"),
+    ((0 , xF86XK_AudioRaiseVolume),                  spawn "/usr/bin/pulseaudio-ctl up"),--"amixer set Master on && amixer set Headphone on && amixer set Master 2+"),
+    ((0 , xF86XK_AudioMute),                         spawn "/usr/bin/pulseaudio-ctl mute"),--"amixer set Master toggle && amixer set Headphone on && amixer set Speaker on"),
+    ((mod4Mask , xK_space),                          spawn "/home/fibe/.xmonad/scripts/switchpowgov.sh"),
+    ((0 , xK_Print),                                 spawn "scrot"),
+    ((modMask , xK_f),                               spawn "spacefm")
   ]
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
@@ -222,6 +234,7 @@ myConfig = defaultConfig
       , layoutHook         = myLayout
       , keys               = myKeys
       , mouseBindings      = myMouseBindings
+      , modMask            = myModMask
       , startupHook        = myStartupHook
       , handleEventHook    = myEventHook
       , logHook            = myLogHook
