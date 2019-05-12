6145688276921f61e47dfe144b7edcc584166fd3
FiBe4000 <fibeube@gmail.com>
2019-05-12 15:54:08 +0200

    Change screen order to fit my current setup

--- scripts/setwallpaper.sh
+++ scripts/setwallpaper.sh
@@ -3,5 +3,5 @@
 #path=$HOME/Pictures/arch_wallpaper
 path=$HOME/Pictures/wallpaper/new
 image=$(ls $path | grep -E '(jpg|png)$' | sort -R | tail -1)
-feh --bg-fill $path/$image
-#feh --bg-fill $path/arch5.png
+#feh --bg-fill $path/$image
+feh --bg-fill $path/3.png
